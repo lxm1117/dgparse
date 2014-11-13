04f9eeb74fb3a3dbf8c93526e0c2299da655df52
Rebecca Murphy <rebeccaroisin@gmail.com>
2014-11-13 12:32:59 +0000

    simple xml parser to investigate structure of xml segments

--- dgparse/snapgene.py
+++ dgparse/snapgene.py
@@ -14,19 +14,28 @@ import struct
 import binascii
 import xml.etree.ElementTree as ET
 
+# setting up a dictionary of parsing functions for each segment
+# if not implemented, returns the original section
 decode_dict = {}
 for i in range(15):
     decode_dict[i] = lambda x: x
-decode_dict[0] = lambda x: decode_DNA(x)
-decode_dict[9] = lambda x: decode_descriptor(x)
-decode_dict[10] = lambda x: decode_features(x)
+#decode_dict[0] = lambda x: decode_DNA(x)
+#decode_dict[9] = lambda x: decode_descriptor(x)
+#decode_dict[10] = lambda x: decode_features(x)
+decode_dict[5] = lambda x: decode_primers(x)
+decode_dict[8] = lambda x: decode_primers(x)
+decode_dict[6] = lambda x: decode_primers(x)
+#decode_dict[3] = lambda x: decode_primers(x)
 
 def decode_DNA(data):
+    # dictionary of DNA properties
+    DNA_properties = {}
+
     # DNA sequence
     DNA = data[1:]
-
-    # DNA properties
-    DNA_properties = {}
+    DNA_properties["sequence"] = DNA
+    
+    # other properties
     properties = ord(data[0])
 
     # topology
@@ -83,11 +92,41 @@ def decode_descriptor(data):
 
     return data
 
+def decode_primers(data):
+    primers = {}
+
+    level_0 = {"recentID": str,
+                "sequence": str,
+                "description": str,
+                "name": str,
+                "minContinuousMatchLen": int,
+                "minMeltingTemperature": int,
+    }
+
+    level_1 = {"meltingTemperature": int,
+                "boundStrand": int,
+                "simplified": bool,
+                "location": str,
+                "annealedBases": str,
+
+    }
+
+    features = ET.fromstring(data)
+    for feature in features:
+        print "level one", feature, feature.attrib
+        for feat in feature:
+            print "level two", feat, feat.attrib
+            for f in feat:
+                print "level three", f, f.attrib
+                for ff in f:
+                    print "level four!", ff, ff.attrib
+                
+
 def decode_features(data):
     all_features = []
 
-    # this is hacky
-    # dictionary would be better
+    # this is hacky -- what is best structure to store parsed data?
+    # dictionary would be better?
     top_level = {"name": str,
                 "swappedSegmentNumbering": bool,
                 "allowSegmentOverlaps": bool,
@@ -100,6 +139,7 @@ def decode_features(data):
                 "hitsStopCodon": bool
     }
 
+    
     second_level = {"color": str, "range": str, "type": str}
 
     features = ET.fromstring(data)
@@ -151,14 +191,16 @@ def decode_features(data):
         print dic
     return data
 
-def decode(seg, data):
-    func = decode_dict[seg]
+def decode(seg, data, parsers):
+    # select correct parsing function
+    func = parsers[seg]
     data = func(data)
     return data
 
 
 def main():
     # TODO
+    # standardize return 
     with open("../data/snapgene/pDONR223 empty vector.dna", "rb") as f:
         segment = f.read(5)
         while segment:
@@ -167,7 +209,7 @@ def main():
             print seg, seg_len
             data = f.read(seg_len)
             #print data
-            snoof = decode(seg, data)
+            snoof = decode(seg, data, decode_dict)
             #if seg == 0:
             #    print snoof
             segment = f.read(5)
