e23781c36fd71a9bfb3bbd616a8955bf3beb2520
Riley Doyle <rileyd@deskopgenetics.com>
2015-07-05 23:37:20 +0100

    adapt snapgene to dtg schema

--- dgparse/snapgene/__init__.py
+++ dgparse/snapgene/__init__.py
@@ -1,9 +1,101 @@
 
 import hashlib
-
 from .main import main, parse_snapgene
 from ..exc import ParserException
 
+# Explicitly define our mapping
+STRAND = [0, 1, -1, 0]
+
+
+
+def extract_sequence(snap_data):
+    """
+    Adapt a snapgene sequence to a deskgen sequence
+    :return:
+    """
+    bases = snap_data['DNA'].pop('sequence').upper()  # normalize case
+    sha1 = hashlib.sha1(bases).hexdigest()
+    return {'bases': bases, 'sha1': sha1, 'type_': "dnamoleculesequence"}
+
+def extract_feature_category(snapfeat):
+    feature_type = snapfeat.pop('type')
+    return {
+        'type_': 'dnafeaturecategory',
+        'name': feature_type,
+        'accession': feature_type.lower(),  # normalize caps
+    }
+
+
+def extract_feature(annotation_data):
+    """
+    Extract the underlying feature
+    :return:
+    """
+    name = annotation_data.pop('name')
+    category = extract_feature_category(annotation_data)
+    if category['accession'] == 'source':
+        return  # No source features
+    accession = category['name'] + '-' + name
+    return {
+        'type_': 'dnafeature',
+        'name': name,
+        'category': category,
+        'accession': accession,
+        'properties': annotation_data
+    }
+
+def extract_coordinates(annotation_data):
+    """
+    Split range and return coordinates
+    :return:
+    """
+    # keep segment as it contains additional data
+    range = annotation_data['Segment']['range'].split('-')
+    # may need to subtract one
+    directionality = annotation_data.pop('directionality', 0)
+    start = int(range[0])
+    end = int(range[0])
+    return start, end, STRAND[directionality]
+
+def extract_annotation(annotation_data):
+    """
+    Extract the list of annotated features in this sequence
+    :return:
+    """
+    # TODO this object will also need properties to store formatting info
+    start, end, strand = extract_coordinates(annotation_data)
+    return {
+        'type_': 'dnamolecule_dnafeature',
+        'start': start,
+        'end': end,
+        'strand': strand,
+        'dnafeature': extract_feature(annotation_data),
+    }
+
+def extract_molecule(molecule):
+    """
+    Extract the DNA molecule own data
+    :param molecule:
+    :return:
+    """
+    is_circular = (molecule['DNA'].pop('topology') == 'circular')
+    annotations = map(extract_annotation, molecule.pop('features'))
+    description = molecule.pop('descriptor')
+    properties = molecule.pop('notes')
+    more_props = molecule.pop('other_properties')
+    sequence = extract_sequence(molecule)
+    seq_props = molecule.pop('DNA')  # get the remaining items
+    properties.update(more_props)
+    properties.update(seq_props)
+    return {
+        'type_': 'dnamolecule',
+        'sequence': sequence,
+        'length': len(sequence['bases']),
+        'dnafeatures': annotations,  # using updated naming
+        'description': description,
+        'is_circular': is_circular,
+        'properties': properties  # combined properties
+    }
 
 
 def parse(open_file):
@@ -12,28 +104,6 @@ def parse(open_file):
     :param open_file:
     :return:
     """
-    result = parse_snapgene(open_file)
-    try:
-        snap_dna = result.pop('DNA')
-        bases = snap_dna.pop('sequence').upper()  # normalize case
-        properties = result.pop('other_properties')
-        properties.update()
-        # adapt topology
-        result['is_circular'] = (snap_dna.get('topology', 'circular')
-                                 == 'circular')
+    snap_result = parse_snapgene(open_file)
+    return extract_molecule(snap_result)
 
-        # remove 'source' as these are not real features.
-        def drop_source(feature_dict):
-            if feature_dict.get('category') != 'source':
-                return True
-        result['features'] = filter(drop_source, result['features'])
-    except KeyError:
-        raise ParserException('No sequence could be parsed')
-    result.update({
-        'sequence': {
-            'bases': bases,
-            'sha1': hashlib.sha1(bases).hexdigest(),
-        },
-        'properties': properties,
-    })
-    return result
