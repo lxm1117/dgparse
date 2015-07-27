6f046d15657a0973c3d42676c210d110b4c7fa72
Riley Doyle <rileyd@deskopgenetics.com>
2015-07-27 22:45:59 +0100

    more exceptions

--- dgparse/exc.py
+++ dgparse/exc.py
@@ -2,11 +2,32 @@
 Parser Exceptions shared by all Parsers
 """
 
+from marshmallow import ValidationError
+
 class ParserException(Exception):
     pass
+
 
 class FormatException(ParserException):
     """
     Use for badly formatted files
     """
     pass
+
+
+class UndefinedRecordType(FormatException):
+    """
+    Use when no record type is stated or inferred.
+    """
+
+
+class NonRecordEntry(FormatException):
+    """
+    Use when a CSV or Excel file contains extraneous records
+    """
+
+
+class IllegalCharacter(ValidationError):
+    """
+    Raise when a disallowed character is present
+    """
