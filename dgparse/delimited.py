faba3eeb1e09340b7ba780a68a612e59b3d43171
Riley Doyle <rileyd@deskopgenetics.com>
2015-07-27 22:45:30 +0100

    add parser for delimited files

--- dgparse/delimited.py
+++ dgparse/delimited.py
@@ -6,16 +6,25 @@ Centralized Parser for models coming from delimiter separated value files
 
 import csv
 import os
+import functools
 
-from dgparse import exc
 
-def clean_record(record):
-    if '' in record:  # blank key
-        msg = "{0} contains a NonRecord Entry {1}".format(open_file.name, record)
-        raise exc.NonRecordEntry(msg)
-    record.update({"type_": record_type})
-    else:
-        return record
+def clean_record(basename, record):
+    result = {'type_': basename.split('.')[0]}
+    for key, value in record.iteritems():
+        if key is '':
+            msg = "{0} contains a NonRecord Entry {1}".format(basename, record)
+            result['ERROR'] = msg
+        if '.' in key:
+            parent_attr, child_attr = key.split('.')
+            if parent_attr in result:
+                result[parent_attr][child_attr] = value
+            else:
+                result[parent_attr] = {child_attr: value}
+        else:
+            result[key] = value
+    return result
+
 
 def parse(open_file, fieldnames=None, record_type=None):
     """
@@ -23,13 +32,9 @@ def parse(open_file, fieldnames=None, record_type=None):
     :param open_file:
     :return:
     """
-    records = []  # always return a list
     # What does this file contain?
     if not record_type:
         record_type = os.path.basename(open_file.name)
+    record_cleaner = functools.partial(clean_record, record_type)
+    return map(record_cleaner, csv.DictReader(open_file, fieldnames))
 
-    for record in csv.DictReader(open_file, fieldnames):
-
-
-        records.append(record)
-    return records
